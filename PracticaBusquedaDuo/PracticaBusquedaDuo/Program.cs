dbe965aa68948491338049647bb8ed13b3112287
rjez12 <jesserjrch@uamv.edu.ni>
2025-11-21 01:39:29 -0600

    Implementa logica del algoritmo de busqueda binaria por autor

--- PracticaBusquedaDuo/PracticaBusquedaDuo/Program.cs
+++ PracticaBusquedaDuo/PracticaBusquedaDuo/Program.cs
@@ -3,13 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PracticaBusquedaDuo.Clases;
 
 namespace PracticaBusquedaDuo
 {
     internal class Program
     {
-        static void Main(string[] args)
+        //Búsqueda Binaria (Por Autor)
+        public static Libro BuscarLibroPorAutorBinaria(List<Libro> libros, string autorBuscado)
         {
+            int izquierda = 0;
+            int derecha = libros.Count - 1;
+
+            while (izquierda <= derecha)
+            {
+                int medio = (izquierda + derecha) / 2;
+
+                // Comparamos el autor del medio con el buscado
+                // StringComparison.OrdinalIgnoreCase ignora mayúsculas/minúsculas
+                int comparacion = string.Compare(libros[medio].Autor, autorBuscado, StringComparison.OrdinalIgnoreCase);
+
+                if (comparacion == 0)
+                {
+                    return libros[medio]; // ¡Encontrado!
+                }
+                else if (comparacion < 0)
+                {
+                    // El autor buscado es "mayor" alfabéticamente, buscamos en la mitad derecha
+                    izquierda = medio + 1;
+                }
+                else
+                {
+                    // El autor buscado es "menor", buscamos en la mitad izquierda
+                    derecha = medio - 1;
+                }
+            }
+
+            return null; // Retornamos null si no se encuentra
         }
     }
 }
